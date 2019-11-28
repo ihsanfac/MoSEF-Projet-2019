6549d41db7bf5f91138cbc3feb94143891b6d8ec
mtir <ihsanmtir@gmail.com>
2019-11-28 16:58:57 +0100

    deuxieme partie

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -5,7 +5,8 @@ echo "Welcome $USER, la date d'aujourd'hui est $djour"
 read -p 'Vous voulez ouvrir quel repertoire svp?' dossier
 
 echo -e "Le repertoire selectionné est" $dossier.\n
-ls $dossier
+ls $dossier/$1
+
 
 
 
