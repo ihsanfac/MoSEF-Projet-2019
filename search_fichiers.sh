b1b1da17231817f63944b8ae5d7fd5ffe594501c
mtir <ihsanmtir@gmail.com>
2019-11-28 17:36:20 +0100

    new

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -5,7 +5,9 @@ echo "Welcome $USER, la date d'aujourd'hui est $djour"
 read -p 'Vous voulez ouvrir quel repertoire svp?' dossier
 
 echo -e "Le repertoire selectionné est" $dossier.\n
-ls $dossier/$1
+grep -il $2 $dossier/$1
+
+
 
 
 
